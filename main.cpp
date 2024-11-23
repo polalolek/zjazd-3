e8cf863e85b38d3dc502819b968a6572973cddd4
polalolek <polakundo@gmail.com>
2024-11-23 12:26:38 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,22 +2,35 @@
 
 int main()
 {
-    char literka;
-    std::cout << "Program do zgadywania literki:) \n";
+    std::cout << "Program do obliczania delty i jej pierwiastkow. \n";
+    std::cout << "Postac rownania kwadratowego: ax^2+bx+c=0 \n";
+    float a, b, c;
+    std::cout << "Podaj a: ";
+    std::cin >> a;
+    std::cout << "Podaj b: ";
+    std::cin >> b;
+    std::cout << "Podaj c: ";
+    std::cin >> c;
+    std::cout << "Rownanie: " << a << "^2+" << b << "x+" << c << "=0 \n";
 
-    do
+    float delta = b * b - 4 * a * c;
+    std::cout << "Delta wynosi: \n"
+              << delta << "\n";
+    float x1, x2, x0;
+    if (delta < 0)
     {
-        std::cout << "Podaj litere: \n";
-        std::cin >> literka;
-
-        if (literka != 't')
-        {
-            std::cout << "Zla literka! \n";
-        }
-
-    } while (literka != 't');
-
-    std::cout << "Zgadlxs literke! \n";
-
+        std::cout << "Rownanie nie ma pierwiastkow.";
+    }
+    else if (delta == 0)
+    {
+        x0 = -b / 2 * a;
+        std::cout << "Rownanie ma jeden pierwiastek, x= " << x0 << "\n";
+    }
+    else if (delta > 0)
+    {
+        x1 = -b - std::sqrt(delta) / 2 * a;
+        x2 = -b + std::sqrt(delta) / 2 * a;
+        std::cout << "Rownanie ma dwa pierwiastki, x1= " << x1 << ", x2= " << x2 << "\n";
+    }
     return 0;
 }
